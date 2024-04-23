0c25255e33218bbcfe98acc1fa14b3673b957ce1
Juan Arnaboldi <nocto2014@gmail.com>
2024-04-23 09:44:06 -0300

    reverse to where nav was normal

--- src/content/nav.tsx
+++ src/content/nav.tsx
@@ -23,7 +23,6 @@ import {
     useDisclosure,
 } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
-import MountImg from "../featured/mountImg";
 
 const OpenModal = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -93,44 +92,6 @@ const OpenModal = () => {
     );
 };
 
-const AnotherModal = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const { t } = useTranslation();
-
-    return (
-        <>
-            <Button
-                onClick={onOpen}
-                style={{
-                    margin: "0",
-                    padding: "0",
-                    backgroundColor: "rgba(0,0,0,0)",
-                    color: "white",
-                }}
-                fontWeight={"normal"}
-                boxSize={"auto"}
-            >
-                MountImg
-            </Button>
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                <ModalOverlay />
-                <ModalContent backgroundColor="rgba(0,0,0,0.9)" color="white">
-                    <ModalHeader>MountImg</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <MountImg />
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme="blue" mr={3} onClick={onClose}>
-                            {t("close")}
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    );
-}
-
 const Nav: React.FC = () => {
     const { colorMode } = useColorMode();
     const { t } = useTranslation();
@@ -186,9 +147,6 @@ const Nav: React.FC = () => {
             </Accordion>
             <Box>
                 {OpenModal()}
-            </Box>
-            <Box borderTop={"1px"} borderColor={"white"}>
-                {AnotherModal()}
             </Box>
         </Box>
     );
